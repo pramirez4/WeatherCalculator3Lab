26dd28cfebbc40472f50fbdaa8d54f6e023815b8
pramirez4 <59981840+pramirez4@users.noreply.github.com>
2020-02-05 20:14:17 -0600

    loopAdded

--- WeatherCalculator3/WeatherCalculator3/Program.cs
+++ WeatherCalculator3/WeatherCalculator3/Program.cs
@@ -15,6 +15,11 @@ namespace WeatherCalculator3
             double heatIndex;
             string input = "";
 
+
+            bool choice = true;
+          
+            while (choice)
+            {
             // Get the temperature from the user
             Console.WriteLine("Enter the temperature");
 
@@ -23,44 +28,58 @@ namespace WeatherCalculator3
             // convert "input" string variable  to an integer and store in "temperature" variables
             temperature = int.Parse(input);
 
-            if (temperature > 50)
-            {
-                // Get the relative humidity from the user
-                Console.WriteLine("Enter the relative humidity");
+                if (temperature > 50)
+                {
+                    // Get the relative humidity from the user
+                    Console.WriteLine("Enter the relative humidity");
 
-                // NOTE: reusing the "input" variable to get the relative humidity
-                input = Console.ReadLine();
+                    // NOTE: reusing the "input" variable to get the relative humidity
+                    input = Console.ReadLine();
 
-                // convert "input" string variable to an integer and store in "relativeHumidity" variable
-                relativeHumidity = int.Parse(input);
+                    // convert "input" string variable to an integer and store in "relativeHumidity" variable
+                    relativeHumidity = int.Parse(input);
 
-                // Calculate the heat index
-                // 0.5 * (T + 61.0 + ((T-68.0)*1.2) + (RH*0.094))
-                heatIndex = 0.5 * (temperature + 61.0 + ((temperature - 68.0) * 1.2) + (relativeHumidity * 0.094));
+                    // Calculate the heat index
+                    // 0.5 * (T + 61.0 + ((T-68.0)*1.2) + (RH*0.094))
+                    heatIndex = 0.5 * (temperature + 61.0 + ((temperature - 68.0) * 1.2) + (relativeHumidity * 0.094));
 
-                // Write the heat index out to the console
-                Console.WriteLine("The heat index is " + heatIndex);
-            }
-            else
-            {
-                // Get the wind 49speed from the user
-                Console.WriteLine("Enter the wind speed");
+                    // Write the heat index out to the console
+                    Console.WriteLine("The heat index is " + heatIndex);
+                }
+                else
+                {
+                    // Get the wind 49speed from the user
+                    Console.WriteLine("Enter the wind speed");
 
-                // NOTE: reusing the "input" variable to get the wind speed
-                input = Console.ReadLine();
+                    // NOTE: reusing the "input" variable to get the wind speed
+                    input = Console.ReadLine();
 
-                // convert "input" string variable to an integer and store in "windSpeed" variable
-                windSpeed = int.Parse(input);
+                    // convert "input" string variable to an integer and store in "windSpeed" variable
+                    windSpeed = int.Parse(input);
 
-                // Calculate the wind chill
-                windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
+                    // Calculate the wind chill
+                    windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
 
-                // Write the windchill out to the console
-                Console.WriteLine("The wind chill is " + windChill);
+                    // Write the windchill out to the console
+                    Console.WriteLine("The wind chill is " + windChill);
+                }
+                Console.WriteLine("Would you like to continue? Yes or No");
+                    input = Console.ReadLine();
+                    if (input == "yes")
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        // Tell the user the program is ending
+                    Console.WriteLine("Program has ended...Thank you!");
+                    choice = false;
+                    }
+            
+           
             }
             
-            // Tell the user the program is ending
-            Console.WriteLine("Program has ended...Thank you!");
+           
         }
     }
 }
